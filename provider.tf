4db7ef0562e46014e49492e6663d5fbded84f1e0
doruboyina <manjunath.imagin@gmail.com>
2018-06-06 09:33:47 +0000

    deleted paths.tf and add content in provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,10 @@
 provider "aws" {
 region     = "us-west-2"
 }
-
+variable "paths" {
+  default = ["/opt/apache-tomcat-9.0.5/logs/catalina.2018-03-19.log" ,"/aws/lambda/lamda-html"]
+  type = "list"
+}
 module "cw_elasticsearch" {
   source = "git@github.com:doruboyina/terraform.git//cw_es"
 }
