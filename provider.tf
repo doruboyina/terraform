74c2ac4b59d74fd4794d47b3fc9e3dda3075069d
doruboyina <manjunath.imagin@gmail.com>
2018-06-08 09:50:13 +0000

    added path in module

--- provider.tf
+++ provider.tf
@@ -1,11 +1,8 @@
 provider "aws" {
 region     = "us-west-2"
 }
-variable "paths" {
-  default = ["/opt/apache-tomcat-9.0.5/logs/catalina.2018-03-19.log" ,"/aws/lambda/lamda-html"]
-  type = "list"
-}
 module "cw_elasticsearch" {
   source = "git@github.com:doruboyina/terraform.git//cw_es"
+  paths = ["/opt/apache-tomcat-9.0.5/logs/catalina.2018-03-19.log", "/aws/lambda/lamda-html"]
 }
 
