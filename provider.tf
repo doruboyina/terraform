c5d97d72be5ab7cc07f74897896a390ebc4b1483
doruboyina <manjunath.imagin@gmail.com>
2018-06-08 11:29:10 +0000

    added provider.tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,7 @@ region     = "us-west-2"
 }
 
 module "cw_elasticsearch" {
-  source = "/root/softwares/watch_elastic/cw_es/lambda"
+  source = "git@github.com:doruboyina/terraform.git//cw_es"
+  paths = ["/opt/apache-tomcat-9.0.5/logs/catalina.2018-03-19.log" ,"/aws/lambda/lamda-html"]
 }
 
